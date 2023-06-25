c9078f08b9a7ec25c05a58829ede8be53cd5faf7
sheodox <imagamer27@msn.com>
2023-06-24 22:03:59 -0500

    Fix community filtering when scrolling on communities
    It wasn't properly passing the query string, so infinite scrolling on communities would stop being filtered to that community after the initial page.

--- src/routes/(app)/c/[communityName]/+page.svelte
+++ src/routes/(app)/c/[communityName]/+page.svelte
@@ -1,7 +1,6 @@
 <PostsPage on:more={more} postViews={data.posts} siteView={data.site.site_view} communityView={data.communityView} />
 
 <script lang="ts">
-	import { Stack } from 'sheodox-ui';
 	import type { PostView } from 'lemmy-js-client';
 	import PostsPage from '$lib/feeds/posts/PostsPage.svelte';
 
@@ -10,7 +9,7 @@
 
 	async function more() {
 		const nextPage: { posts: PostView[] } = await fetch(
-			`/api/posts?page=${++page}?communityName=${data.communityName}`
+			`/api/posts?page=${++page}&communityName=${data.communityName}`
 		).then((res) => res.json());
 		data.posts = [...data.posts, ...nextPage.posts.filter((p) => !data.posts.some((p2) => p2.post.id === p.post.id))];
 	}
