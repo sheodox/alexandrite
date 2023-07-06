4e6327832e5a747ba30b33d4388ca38c9d4aaec5
sheodox <imagamer27@msn.com>
2023-07-06 16:52:19 -0500

    Add some more feedback when deleting posts
    This disables the button and adds a spinner like on many other pages.

--- src/routes/(app)/c/[communityName]/post/[postId]/delete/+page.svelte
+++ src/routes/(app)/c/[communityName]/post/[postId]/delete/+page.svelte
@@ -15,17 +15,27 @@
 	<p>Are you sure you want to delete this post?</p>
 
 	<Stack dir="r" gap={2}>
-		<form use:enhance method="POST" action="?/deletePost">
-			<button class="danger">Delete</button>
+		<form use:enhance method="POST" action="?/deletePost" on:submit={() => (submitting = true)}>
+			<button class="danger f-row gap-2" disabled={submitting}>
+				{#if submitting}
+					<Spinner />
+				{/if}
+				Delete</button
+			>
 		</form>
-		<a class="button tertiary" href="/post/{data.postView.post.id}">Cancel</a>
+		{#if !submitting}
+			<a class="button tertiary" href="/post/{data.postView.post.id}">Cancel</a>
+		{/if}
 	</Stack>
 </Stack>
 
 <script lang="ts">
 	import { Stack } from 'sheodox-ui';
 	import Post from '$lib/feeds/posts/Post.svelte';
+	import Spinner from '$lib/Spinner.svelte';
 	import { enhance } from '$app/forms';
 
 	export let data;
+
+	let submitting = false;
 </script>
