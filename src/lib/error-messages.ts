c1a4b00507cbb0d64dd7088d5c757912eb72141a
sheodox <imagamer27@msn.com>
2023-07-06 16:29:42 -0500

    Added an error message for when someone tries logging in with an invalid username

--- src/lib/error-messages.ts
+++ src/lib/error-messages.ts
@@ -2,7 +2,8 @@ import type { HttpError } from '@sveltejs/kit';
 
 export const errorMessages: Record<string, string> = {
 	email_not_verified: 'Email not verified',
-	password_incorrect: 'Password incorrect'
+	password_incorrect: 'Password incorrect',
+	couldnt_find_that_username_or_email: "Couldn't find that username or email"
 };
 
 export function getMessageFromError(e: unknown) {
