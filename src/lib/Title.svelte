9eadb424b78c81d7fa395e2418465a2ec2a634d4
sheodox <imagamer27@msn.com>
2023-07-07 15:14:06 -0500

    Show how many unread items you have in the page title

--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -3,9 +3,24 @@
 </svelte:head>
 
 <script lang="ts">
+	import { getAppContext } from './app-context';
+
 	export let title = '';
 
 	const appName = 'Alexandrite';
+	const { loggedIn, unreadCount } = getAppContext();
 
-	$: fullTitle = title ? title + ' - ' + appName : appName;
+	$: fullTitle = (function () {
+		const segments = [];
+		if (loggedIn && $unreadCount > 0) {
+			segments.push(`(${$unreadCount})`);
+		}
+
+		if (title) {
+			segments.push(title);
+		}
+
+		segments.push(appName);
+		return segments.join(' - ');
+	})();
 </script>
