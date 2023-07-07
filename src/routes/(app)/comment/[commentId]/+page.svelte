31761fa14c16173c13c253b698900592e6238012
sheodox <imagamer27@msn.com>
2023-07-06 22:21:04 -0500

    Mirror the post page's layout sizing.
    The page is identical save for how comments show, the post should look the same.

--- src/routes/(app)/comment/[commentId]/+page.svelte
+++ src/routes/(app)/comment/[commentId]/+page.svelte
@@ -1,12 +1,15 @@
 {#if data.postView}
 	<Title title={`Comment on "${data.postView.post.name}"`} />
-	<PostPage postView={data.postView} commentViews={data.commentViews} rootCommentId={data.commentId} />
+	<Layout size="wide">
+		<PostPage postView={data.postView} commentViews={data.commentViews} rootCommentId={data.commentId} />
+	</Layout>
 {:else}
 	<Title title="Not Found" />
 	<p>Post not found!</p>
 {/if}
 
 <script>
+	import { Layout } from 'sheodox-ui';
 	import PostPage from '$lib/PostPage.svelte';
 	import Title from '$lib/Title.svelte';
 
