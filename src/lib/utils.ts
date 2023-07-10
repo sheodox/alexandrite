1d89db5ef3f464ffc8fb0202ad7304a575cb1322
sheodox <imagamer27@msn.com>
2023-07-10 18:44:36 -0500

    Remove endpoint, it's unused now with no server code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,21 +16,6 @@ export class Throttler {
 	}
 }
 
-export const endpoint = (pathname: string, queryParams: Record<string, string | number | null | undefined>) => {
-	const url = pathname,
-		query = [];
-
-	// sort alphabetically for consistent ordering
-	const params = Object.entries(queryParams).sort((a, b) => a[0].localeCompare(b[0]));
-
-	for (const [name, value] of params) {
-		if (value !== undefined && value !== null && value !== '') {
-			query.push(`${name}=${encodeURIComponent(value)}`);
-		}
-	}
-	return url + (query.length ? '?' + query.join('&') : '');
-};
-
 // wrapper for doing things with forms/api methods
 export type ActionFn = (body: Record<string, FormDataEntryValue>) => Promise<unknown>;
 
