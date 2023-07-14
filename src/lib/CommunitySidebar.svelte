21f435ad663889b75a64e13ced9cfc11324cddb4
sheodox <imagamer27@msn.com>
2023-07-14 14:35:32 -0500

    Better community links
    The link to your instance's page for the community is not as hidden (not just an icon anymore, actual inline link), and now you can click the community name to go to the community, which is a nice shortcut when you're viewing a post and it shows the limited view of the community.

--- src/lib/CommunitySidebar.svelte
+++ src/lib/CommunitySidebar.svelte
@@ -1,16 +1,21 @@
 <article>
 	<Sidebar description={community.description ?? ''} bannerImageSrc={community.banner} context="Community">
-		<span slot="name">
+		<a href="/c/{nameAtInstance(community)}" slot="name">
 			<NameAtInstance place={community} prefix="!" />
-			<ExternalLink href={community.actor_id}>
-				<Icon icon="arrow-up-right-from-square" />
-			</ExternalLink>
-		</span>
+		</a>
 
 		<div slot="actions">
-			{#if communityView}
-				<CommunityCounts {communityView} />
-			{/if}
+			<Stack dir="c" gap={2}>
+				{#if communityView}
+					<CommunityCounts {communityView} />
+				{/if}
+				<Stack dir="r" gap={2} align="center">
+					<ExternalLink href={community.actor_id} cl="inline-link">
+						<Icon icon="arrow-up-right-from-square" variant="icon-only" />
+						View on {communityInstance}
+					</ExternalLink>
+				</Stack>
+			</Stack>
 		</div>
 		<div slot="end">
 			{#if moderators}
@@ -34,8 +39,11 @@
 	import CommunityCounts from './CommunityCounts.svelte';
 	import UserLink from './UserLink.svelte';
 	import type { CommunityView, Community, CommunityModeratorView } from 'lemmy-js-client';
+	import { nameAtInstance } from './nav-utils';
 
 	export let community: Community;
 	export let communityView: CommunityView | null = null;
 	export let moderators: CommunityModeratorView[] | null = null;
+
+	$: communityInstance = new URL(community.actor_id).host;
 </script>
