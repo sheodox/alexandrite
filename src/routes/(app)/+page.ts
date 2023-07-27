1455f43de1c30123eaec028f6a0ad9629ae7bc4b
sheodox <imagamer27@msn.com>
2023-07-26 19:49:46 -0500

    Redirect to the correct instance route when visiting `/`

--- src/routes/(app)/+page.ts
+++ src/routes/(app)/+page.ts
@@ -1,9 +1,9 @@
 import { redirect } from '@sveltejs/kit';
 import { get } from 'svelte/store';
 import type { PageLoad } from './$types';
-import { defaultInstance } from '$lib/profiles/profiles';
+import { instance } from '$lib/profiles/profiles';
 
 export const load = (async () => {
-	const instance = get(defaultInstance);
-	throw redirect(303, `/${instance}`);
+	const inst = get(instance);
+	throw redirect(303, `/${inst}`);
 }) satisfies PageLoad;
