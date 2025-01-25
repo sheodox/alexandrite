7cdfe6d889e0317afbc569b417ae949b3d9b35b8
sheodox <3468630+sheodox@users.noreply.github.com>
2025-01-25 13:38:09 -0600

    Support different ways of showing uploaded media

--- src/lib/UploadedMedia.svelte
+++ src/lib/UploadedMedia.svelte
@@ -25,35 +25,47 @@
 	}
 </style>
 
-{#if upload.url}
+{#if currentUploadUrl || upload?.url}
 	<div class="upload-preview">
 		<div class="upload-url f-row align-items-center">
-			<span class="url">{upload.url}</span>
-
+			<span class="url">{upload?.url ?? currentUploadUrl}</span>
 			<IconButton
-				on:click={() => upload.url && copyToClipboard(upload.url)}
+				on:click={onCopyClick}
 				cl="tertiary mr-1"
 				icon="copy"
 				text="Copy URL"
 				type="button"
 				disabled={deleting}
 			/>
-			<IconButton
-				on:click={() => deleteUpload(upload)}
-				cl="delete-upload danger mr-1"
-				icon="trash"
-				text="Delete Image"
-				type="button"
-				disabled={deleting}
-				busy={deleting}
-			/>
+			{#if upload}
+				<IconButton
+					on:click={() => deleteUpload(upload)}
+					cl="delete-upload danger mr-1"
+					icon="trash"
+					text="Delete Image"
+					type="button"
+					disabled={deleting}
+					busy={deleting}
+				/>
+			{:else if currentUploadUrl}
+				<IconButton
+					on:click={() => (currentUploadUrl = '')}
+					cl="mr-1 tertiary"
+					icon="times"
+					text="Remove"
+					type="button"
+				/>
+			{/if}
 		</div>
 		<div class="media-preview">
-			{#if hasImageExtension(upload.url)}
-				<Image src={upload.url} resizable={false} loadingHeight="20rem" />
-			{:else if hasVideoExtension(upload.url)}
+			{#if hasImageExtension(upload?.url ?? currentUploadUrl)}
+				<Image src={upload?.url ?? currentUploadUrl} resizable={false} loadingHeight="20rem" />
+			{:else if upload && hasVideoExtension(upload.url)}
 				<!-- svelte-ignore a11y-media-has-caption -->
 				<video src={upload.url} controls />
+			{:else if currentUploadUrl}
+				<Image src={currentUploadUrl} resizable={false} loadingHeight="20rem" />
+				<Avatar src={currentUploadUrl} icon="user-circle" size="4rem" />
 			{/if}
 		</div>
 	</div>
@@ -68,14 +80,24 @@
 	import { profile } from './profiles/profiles';
 	import { createAutoExpireToast } from 'sheodox-ui';
 	import { hasImageExtension, hasVideoExtension } from './feeds/posts/post-utils';
+	import Avatar from './Avatar.svelte';
 
-	export let upload: UploadImageResponse;
+	export let upload: UploadImageResponse | null = null;
+	// the url of the current avatar/icon/etc, if we don't have the upload response
+	export let currentUploadUrl = '';
 
 	let deleting = false;
 
 	const dispatch = createEventDispatcher<{
 		'delete-upload': UploadImageResponse;
 	}>();
+
+	function onCopyClick() {
+		const text = upload?.url ?? currentUploadUrl;
+		if (text) {
+			copyToClipboard(text);
+		}
+	}
 
 	async function deleteUpload(upload: UploadImageResponse) {
 		if (!upload.delete_url) {
