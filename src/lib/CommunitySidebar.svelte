7e7abb771cf63e6153128a60ee832be21fcd7ec8
sheodox <3468630+sheodox@users.noreply.github.com>
2025-01-25 23:01:05 -0600

    Made the spacing for moderators and admins accordions the same.

--- src/lib/CommunitySidebar.svelte
+++ src/lib/CommunitySidebar.svelte
@@ -26,50 +26,52 @@
 			<div slot="end">
 				<Accordion bind:open={$communityDetailsOpen}>
 					<span slot="title"><Icon icon="users" /> Community Details</span>
-					<ul class="sx-list mb-2">
-						{#each communityStats as stat}
-							<li class="sx-list-item two-columns">
-								<span class="column">{stat.label}</span>
-								<span class="column text-align-right">{stat.value.toLocaleString()} <Icon icon={stat.icon} /></span>
+					<div class="f-column gap-2">
+						<ul class="sx-list">
+							{#each communityStats as stat}
+								<li class="sx-list-item two-columns">
+									<span class="column">{stat.label}</span>
+									<span class="column text-align-right">{stat.value.toLocaleString()} <Icon icon={stat.icon} /></span>
+								</li>
+							{/each}
+							<li class="sx-list-item">
+								<ModlogLink
+									communityId={community.id}
+									highlight={userModerates ?? false}
+									highlightColor="green"
+									warn={warnModlog}
+								/>
 							</li>
-						{/each}
-						<li class="sx-list-item">
-							<ModlogLink
-								communityId={community.id}
-								highlight={userModerates ?? false}
-								highlightColor="green"
-								warn={warnModlog}
-							/>
-						</li>
-						<li class="sx-list-item">
-							<ExternalLink href={community.actor_id} cl="inline-link">
-								<Icon icon="arrow-up-right-from-square" />
-								View on {communityInstance}
-							</ExternalLink>
-						</li>
-					</ul>
+							<li class="sx-list-item">
+								<ExternalLink href={community.actor_id} cl="inline-link">
+									<Icon icon="arrow-up-right-from-square" />
+									View on {communityInstance}
+								</ExternalLink>
+							</li>
+						</ul>
 
-					{#if moderators}
-						<Accordion>
-							<span slot="title">Moderators</span>
-							<Stack dir="c" gap={2}>
-								<ul class="sx-list">
-									{#each moderators as mod}
-										<li class="sx-list-item">
-											<ModeratorRow hasSeniority={hasModSeniority(moderators, mod.moderator.id)} {mod} />
-										</li>
-									{/each}
-								</ul>
-								{#if userModerates && !userIsHeadMod}
-									<Stack dir="r" justify="end">
-										<BusyButton small icon="user-minus" busy={$userModResignPending} on:click={resignFromModTeam}
-											>Leave Mod Team</BusyButton
-										>
-									</Stack>
-								{/if}
-							</Stack>
-						</Accordion>
-					{/if}
+						{#if moderators}
+							<Accordion>
+								<span slot="title">Moderators</span>
+								<Stack dir="c" gap={2}>
+									<ul class="sx-list">
+										{#each moderators as mod}
+											<li class="sx-list-item">
+												<ModeratorRow hasSeniority={hasModSeniority(moderators, mod.moderator.id)} {mod} />
+											</li>
+										{/each}
+									</ul>
+									{#if userModerates && !userIsHeadMod}
+										<Stack dir="r" justify="end">
+											<BusyButton small icon="user-minus" busy={$userModResignPending} on:click={resignFromModTeam}
+												>Leave Mod Team</BusyButton
+											>
+										</Stack>
+									{/if}
+								</Stack>
+							</Accordion>
+						{/if}
+					</div>
 				</Accordion>
 			</div>
 		</Sidebar>
