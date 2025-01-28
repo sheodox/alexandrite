0c925259aa1ac58996432215801da41572c4b465
sheodox <3468630+sheodox@users.noreply.github.com>
2025-01-27 21:19:24 -0600

    Make clicking the text also copy, add an animation
    Animation makes it look like something happened.
    
    Lemmy-ui has a similar thing in the community sidebar that selects the text when clicked, having the text be clickable to copy as well makes it an easier click target and feels like something it should do.

--- src/lib/CopyableText.svelte
+++ src/lib/CopyableText.svelte
@@ -8,12 +8,15 @@
 	}
 	.text-preview {
 		font-family: monospace, sans-serif;
+		cursor: pointer;
 	}
 </style>
 
-<p class="copyable-text m-0 px-4 py-2 f-row align-items-center gap-2">
-	<span class="text-preview">{text}</span>
-	<IconButton icon="copy" text="Copy" small on:click={() => copyToClipboard(text)} />
+<p class="copyable-text m-0 px-4 py-2 f-row align-items-center gap-2" bind:this={el}>
+	<!-- ignoring warnings because there's a button for accessibility, but people will want to click the text too -->
+	<!-- svelte-ignore a11y-no-static-element-interactions a11y-click-events-have-key-events -->
+	<span class="text-preview" on:click={copy}>{text}</span>
+	<IconButton icon="copy" text="Copy" small on:click={copy} />
 </p>
 
 <script lang="ts">
@@ -21,4 +24,23 @@
 	import { copyToClipboard } from './utils';
 
 	export let text: string;
+
+	let el: HTMLParagraphElement;
+
+	function copy() {
+		copyToClipboard(text);
+		el.animate(
+			[
+				{
+					background: 'var(--sx-gray-transparent)'
+				},
+				{
+					background: 'var(--sx-gray-dark)'
+				}
+			],
+			{
+				duration: 200
+			}
+		);
+	}
 </script>
